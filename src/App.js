2e16e12539ca85ba86fd227e86df28c2e1603801
Marko Vujanic <crashxx@gmail.com>
2020-03-28 15:26:16 +0100

    remove not needed boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from 'react'
-import {GenericStyles, AppWrapper, MainWrapper} from 'src/styles'
-import {Main} from 'src/styles/layout'
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useParams,
-} from 'react-router-dom'
+import {GenericStyles} from 'src/styles'
+import './App.css'
 import ShortLink from './ShortLink'
 
 function App() {
@@ -18,38 +11,11 @@ function App() {
       .then(data => setAppName(data.name))
   }, [])
   return (
-    <Router>
-      <AppWrapper>
-        <GenericStyles />
-        <MainWrapper>
-          <Switch>
-            <Route exact path="/">
-              <Main>
-                <div className="App">
-                  Connection successuful with {appName}
-                  <ShortLink />
-                </div>
-              </Main>
-            </Route>
-            <Route path="/:id">
-              <div>
-                <RandomDetails />
-              </div>
-            </Route>
-          </Switch>
-        </MainWrapper>
-      </AppWrapper>
-    </Router>
-  )
-}
-
-function RandomDetails() {
-  let {id} = useParams()
-
-  return (
     <div>
-      <h3>ID: {id}</h3>
+      <GenericStyles />
+      <ShortLink />
     </div>
   )
 }
+
 export default App
