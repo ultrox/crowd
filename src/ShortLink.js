6f362b97a27704960439df0358e1a9a61edf02e9
Marko Vujanic <crashxx@gmail.com>
2020-03-28 15:29:36 +0100

    Add jsx for for shortLink component

--- src/ShortLink.js
+++ src/ShortLink.js
@@ -13,27 +13,58 @@ function ShortLink() {
       body: JSON.stringify({orgUrl: orgUrl.value}),
     })
       .then(res => res.json())
-      .then(data => setDisplay(data))
+      .then(resData => setDisplay(resData.data))
     form.reset()
   }
 
   return (
     <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          handleOnSubmit(e.target)
-        }}
-      >
-        <input type="url" placeholder="https://google.com" id="orgUrl" />
-        <button>Short me</button>
-      </form>
-      <div></div>
-      <pre style={{backgroundColor: 'white'}}>
-        {JSON.stringify(display, null, 2)}
-      </pre>
+      <header>
+        <div className="container">
+          <div className="content">
+            <h3 className="logo-text">Crowdly</h3>
+            <p className="slogan">
+              Simplify your life, wash your hands, and don't memorize links
+            </p>
+            <form
+              method="POST"
+              onSubmit={e => {
+                e.preventDefault()
+                handleOnSubmit(e.target)
+              }}
+            >
+              <input
+                className="shorten"
+                type="url"
+                placeholder="https://google.com"
+                id="orgUrl"
+              />
+              <button className="shorten-action">Short</button>
+            </form>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          {display ? (
+            <div className="information">
+              <h4>Information</h4>
+              <p className="full-url">{display.orgUrl}</p>
+              <div className="info-content">
+                <p className="shorted-url">{display.shortLink}</p>
+                <button id="copy">Copy</button>
+
+                {display.visited && (
+                  <div className="visited">visited {display.visited}</div>
+                )}
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </main>
+      <div />
     </div>
   )
-  return <input />
 }
+
 export default ShortLink
